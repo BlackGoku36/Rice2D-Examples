ff6974aa0f963fddd1ebe0bea6ce8165611e8641
BlackGoku36 <urjasvisuthar@gmail.com>
2020-12-16 16:26:41 +0530

    Use submodule instead

--- Rice2D/TODO.md
+++
@@ -1,4 +0,0 @@
-* Test Shaders
-* Test camera
-* Add more to utils and tools
-* Add assets
--- Rice2D/khafile.js
+++
@@ -1,5 +0,0 @@
-let project = new Project('Rice2D2');
-project.addSources('Sources');
-project.addAssets('Assets/**');
-project.addDefine('analyzer-optimize');
-resolve(project);
